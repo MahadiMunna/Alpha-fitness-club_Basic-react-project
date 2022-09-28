3c7abf77d29626c462df8517366624cbfb350c13
MahadiMunna <mahadimunna.official@gmail.com>
2022-09-29 01:29:36 +0600

    styles added in body compo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import logo from './images/logo.png';
 import './App.css';
 import Body from './components/Body/Body';
-import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
     <div>
-      <img className='logo' src={logo} alt="" />
-      <h1>Alpha Fitness Club</h1>
       <Body></Body>
-      <Sidebar></Sidebar>
     </div>
   );
 }
--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import logo from '../../images/logo.png';
+import Posts from '../Posts/Posts';
+import Sidebar from '../Sidebar/Sidebar';
 import './Body.css';
 
 const Body = () => {
     return (
-        <div>
-            <h1>Body</h1>
+        <div className='body-container'>
+            <div className='main-section'>
+                <div className='header'>
+                    <img className='logo' src={logo} alt="" />
+                    <h1>ALPHA-FITNESS-CLUB</h1>
+                </div>
+                <h2>Select today’s exercise</h2>
+                <div>
+                    <Posts></Posts>
+                </div>
+            </div>
+            <div className='sidebar'>
+                <Sidebar></Sidebar>
+            </div>
         </div>
     );
 };
