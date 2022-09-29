19409deb8844504c5eb0b9a432fc068b329d466e
MahadiMunna <mahadimunna.official@gmail.com>
2022-09-29 16:15:22 +0600

    answer component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Answers from './components/Answers/Answers';
 import Body from './components/Body/Body';
 
 function App() {
   return (
     <div>
       <Body></Body>
+      <Answers></Answers>
     </div>
   );
 }
--- src/components/Answers/Answers.js
+++ src/components/Answers/Answers.js
@@ -3,9 +3,13 @@ import './Answers.css'
 
 const Answers = () => {
     return (
-        <div>
-            <h1>1. How does react work?</h1>
-            <p></p>
+        <div className='answers'>
+            <h2>1. How does react work?</h2>
+            <p>- ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props.</p>
+            <h2>2. What is the difference between props and state?</h2>
+            <p>- Props are used to pass data from one component to another. The state is a local data storage that is local to the component only and cannot be passed to other components.</p>
+            <h2>3. What does useEffect do beside data load?</h2>
+            <p>- Perform DOM updates, Data fetching or call imperative API. </p>
         </div>
     );
 };
